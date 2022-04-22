801ad6def813863454b7e0b1b45c992b250c353e
doofensmith <wahyuwijanarko3998@gmail.com>
2022-04-22 13:33:36 +0700

    create project

---
+++ src/main/java/com/softlaboratory/springbootminiproject/SpringbootMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.softlaboratory.springbootminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMiniprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMiniprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softlaboratory/springbootminiproject/SpringbootMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softlaboratory.springbootminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMiniprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
